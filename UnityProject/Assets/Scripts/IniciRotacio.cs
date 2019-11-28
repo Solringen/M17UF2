215c3b2758f860fbf741ef098c3640f4290e0d2d
ivandudnyk <ivan.dudnik.96@gmail.com>
2019-11-28 13:56:33 +0100

    He añadidio escena "Inici" con menu de inici y camera que da vueltas 360º

---
+++ UnityProject/Assets/Scripts/IniciRotacio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IniciRotacio : MonoBehaviour {
+    public float speed = 0.01f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	
+    void Update()
+    {
+       
+
+
+       transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y + speed/10);
+
+    }
+}
