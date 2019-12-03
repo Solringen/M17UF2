c62d1c18b44ff0d94f2ed38650199faf9c65e328
GeorgePlopu <georgeplopu@gmail.com>
2019-12-03 12:40:13 +0100

    5ync

--- UnityProject/Assets/Scripts/Player_Controller.cs
+++ UnityProject/Assets/Scripts/Player_Controller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player_Controller : MonoBehaviour
 {
@@ -38,6 +39,10 @@ public class Player_Controller : MonoBehaviour
             Destroy(other.gameObject);
             damage();
         }
+      else if(other.tag == "scene")
+      {
+      SceneManager.LoadScene("Inicio");
+      }
     }
 
     public void damage()
